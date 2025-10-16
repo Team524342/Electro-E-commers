a5a0880932c1104fdd808b936d5eb0fab5b80585
Shaikbaba1112 <shaikhbaba9539@gmail.com>
2025-10-16 15:37:05 +0530

    replace component code

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} ElectroMart. All Rights Reserved ⚡</p>
+    </footer>
+  );
+}
+
+export default Footer;
