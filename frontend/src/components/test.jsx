2239a0374071e169c8354b12023d26a60295c5c5
Mohd Abdul Ghani <mohdabdulghani177@gmail.com>
2025-12-02 14:36:10 +0530

    feat: update Test component with lucide-react icons and conditional rendering

--- frontend/src/components/test.jsx
+++ frontend/src/components/test.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Heart,HeartHandshakeIcon } from "lucide-react";
+
 export default function Test() {
 const [count, setCount] = useState(1);
 const handleClick = () => {
@@ -9,9 +11,32 @@ const decrement =() =>{
         setCount(count - 1)
     )
 }
+const [showpara, setShowpara] = useState(false);
+const handleClicks =() => {
+    setShowpara (!showpara)
+}
+
+const [counts, setCounts]= useState(0);
+const [like, setLike]= useState(false);
+const liked =( ) => {
+    if (like) {
+        setLike(false);
+        setCounts(counts-1);
+    }
+    else {
+        setLike(true);
+        setCounts(counts+1);
+    }
+}
     return (<div>Test Component count: {count}
         <button type="button" onClick={handleClick}>+</button>
         <button type="button" onClick={decrement} disabled={count===0}> -</button>
+        <button type="button" onClick={handleClicks}>{showpara ? "hide" : "show"}</button>
+        <p>{showpara && "This is a paragraph shown conditionally."  }</p>
+        <button type="button" onClick={liked} >{like ? <Heart size={20} color="red"/> : <Heart size={20} color="green"/>}</button>{counts}
+        
+        <HeartHandshakeIcon />
     </div>
     );
+
 }
